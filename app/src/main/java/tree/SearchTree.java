9c07e2435f544b6c1b52031dc849462affac85a6
chenming <583431043@qq.com>
2018-06-01 11:52:52 +0800

    二叉排序树的复习--递归和循环实现二叉树的创建和查找

---
+++ app/src/main/java/tree/SearchTree.java
@@ -0,0 +1,215 @@
+package tree;
+
+/**
+ * Created by chenming on 2018/6/1
+ * 排序二叉树树的复习,方法尽量提供循环和递归两种方式实现
+ */
+public class SearchTree<T extends Comparable> implements Tree<T> {
+
+    private BinaryNode<T> mRoot;
+
+    public SearchTree() {
+        mRoot = null;
+    }
+
+    @Override
+    public void insert(T data) {
+//        insertByTrans(data);
+        mRoot = insertByRecursion(mRoot, data);
+    }
+
+    /**
+     * 循环插入
+     */
+    private void insertByTrans(T data) {
+        BinaryNode<T> newNode = new BinaryNode<>(data);
+        if (mRoot == null) {
+            mRoot = newNode;
+            return;
+        }
+
+        BinaryNode<T> curNode = mRoot;//从根节点出发
+        BinaryNode<T> parentNode = mRoot;//当前节点的父节点
+        boolean isLeft = false;//标记插入位置是左还是右
+        while (curNode != null) {
+            //循环体：判断大小
+            parentNode = curNode;//保存父节点
+            int compare = data.compareTo(curNode.data);
+            if (compare > 0) {//data较大
+                //curNode往右走
+                isLeft = false;
+                curNode = curNode.right;
+            } else if (compare < 0) {
+                //curNode往左走
+                curNode = curNode.left;
+                isLeft = true;
+            } else {
+                curNode.mDupCount++;
+            }
+        }
+
+        if (isLeft) {
+            parentNode.left = newNode;
+        } else {
+            parentNode.right = newNode;
+        }
+    }
+
+    /**
+     * 递归插入元素
+     * @param node 子树节点
+     * @param data 插入数据
+     */
+    private BinaryNode<T> insertByRecursion(BinaryNode<T> node, T data) {
+        //递归结束条件 node == null表示已经到叶子节点的下面一层了，挂载新节点,返回
+        if(node == null){
+            node = new BinaryNode<>(data);//连接新节点返回给上一层链接
+        }else{//向左右子树递归
+            //递归比较
+            int compare = data.compareTo(node.data);
+
+            if (compare < 0) {//左
+                node.left = insertByRecursion(node.left,data);//由于插入操作需要链接节点,所以需要将低层次的node返回给上一层次的rootNode
+            } else if (compare > 0) {//右
+                node.right = insertByRecursion(node.right,data);
+            } else {//相等
+                node.mDupCount++;//重复计数+1
+            }
+        }
+
+        return node;
+    }
+
+    @Override
+    public BinaryNode findNode(T data) {
+        return findNodeByTrans(data);
+//        return findNodeByRecursion(mRoot, data);
+    }
+
+    /**
+     * 循环查找
+     *
+     * @param data
+     * @return
+     */
+    private BinaryNode findNodeByTrans(T data) {
+        if (mRoot == null) {
+            return null;
+        }
+        BinaryNode<T> curNode = mRoot;
+        while (curNode != null) {
+            int compare = data.compareTo(curNode.data);
+            if (compare > 0) {//data较大
+                //curNode往右走
+                curNode = curNode.right;
+            } else if (compare < 0) {
+                //curNode往左走
+                curNode = curNode.left;
+            } else {//相等,则查找到
+                return curNode;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 递归查找元素
+     *
+     * @param data
+     * @return
+     */
+    private BinaryNode findNodeByRecursion(T data) {
+        return findNodeByRecursion(mRoot, data);
+    }
+
+    /**
+     * 当前子树节点
+     *
+     * @param node
+     * @param data
+     */
+    public BinaryNode findNodeByRecursion(BinaryNode<T> node, T data) {
+        //递归结束条件：子树节点为空 or node.data = data
+        if (node == null) {
+            return null;
+        }
+        int compare = data.compareTo(node.data);
+        if (compare == 0) {//找到匹配元素,返回
+            return node;
+        }
+
+        //否则向左右子树递归
+        if (compare > 0) {//右子树递归
+            return findNodeByRecursion(node.right, data);
+        } else {
+            return findNodeByRecursion(node.left, data);
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int height() {
+        return 0;
+    }
+
+    @Override
+    public String preOrder() {
+        return null;
+    }
+
+    @Override
+    public String inOrder() {
+        return null;
+    }
+
+    @Override
+    public String postOrder() {
+        return null;
+    }
+
+    @Override
+    public String levelOrder() {
+        return null;
+    }
+
+    @Override
+    public void remove(T data) {
+
+    }
+
+    @Override
+    public T findMin() {
+        return null;
+    }
+
+    @Override
+    public T findMax() {
+        return null;
+    }
+
+    /**
+     * 是否包含元素
+     *
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean contains(T data) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
--- app/src/test/java/com/qicode/algorithmlearning/ExampleUnitTest.java
+++ app/src/test/java/com/qicode/algorithmlearning/ExampleUnitTest.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
 
 import List.dlinkedlist.DLinkedList;
 import stack.Stack;
+import tree.BinaryNode;
+import tree.SearchTree;
 
 
 /**
@@ -39,14 +41,14 @@ public class ExampleUnitTest {
         list.add(2);
         list.add(3);
         Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Integer i = iterator.next();
             iterator.remove();
         }
     }
 
     @Test
-    public void testStack(){
+    public void testStack() {
 //        Stack测试
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
@@ -54,11 +56,30 @@ public class ExampleUnitTest {
         stack.push(3);
         stack.pop();
         stack.push(4);
-        System.out.println(stack.size()+"=="+stack.peek());
+        System.out.println(stack.size() + "==" + stack.peek());
 //        stack.clear();
-        while (!stack.isEmpty()){
-            System.out.println(stack.pop()+"");
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + "");
         }
-        System.out.println(stack.size()+"==");
+        System.out.println(stack.size() + "==");
+    }
+
+    @Test
+    public void testSearchTree() {
+        int[] nodes = {8, 10, 6, 5, 7, 9, 11, 1, 2, 3};
+        SearchTree<Integer> tree = new SearchTree<>();
+        for (int i = 0; i < nodes.length; i++) {
+            tree.insert(nodes[i]);
+        }
+        for (int i = 0; i < nodes.length; i++) {
+            BinaryNode<Integer> node = tree.findNode(nodes[i]);
+            System.out.println(node.data);
+            if(node.left != null){
+                System.out.println("    --"+ node.left.data);
+            }
+            if(node.right != null){
+                System.out.println("    --"+ node.right.data);
+            }
+        }
     }
 }
